e5004bdbbb961600428b02c9d68b426fa378ba85
UltWolf <core.safecrlf>
2018-02-08 10:04:12 +0300

    Remake database seed

--- WebApplication14/Program.cs
+++ WebApplication14/Program.cs
@@ -20,23 +20,7 @@ namespace WebApplication14
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationContext>();
-                    var _userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var _roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    var mapper = services.GetRequiredService<IMapper>();
-                    DbInitializer.Initialize(context,_userManager,_roleManager,mapper);//<---Do your seeding here
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+         
             host.Run();
         }
 
