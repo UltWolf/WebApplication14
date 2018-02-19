5eaeb3d7fa47ade9fde33eefdb0e1a2b1ee532ca
UltWolf <core.safecrlf>
2018-02-19 15:59:48 +0300

    added registry services sqrs

---
+++ WebApplication14/Services/DefaultRegistry.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CQRSlite.Commands;
+using CQRSlite.Domain;
+using CQRSlite.Events;
+using CQRSlite.Routing;
+using StackExchange.Redis;
+using System;
+using System.Linq;
+using WebApplication14.Models;
+
+namespace WebApplication14.Services
+{
+    public class DefaultRegistry : Registry
+    {
+
+
+        public DefaultRegistry()
+        {
+            //Commands, Events, Handlers
+            Scan(
+                scan => {
+                    scan.TheCallingAssembly();
+                    scan.AssemblyContainingType<BaseEvent>();
+                    scan.Convention<FirstInterfaceConvention>();
+                });
+
+            //CQRSLite
+            For<InProcessBus>().Singleton().Use<InProcessBus>();
+            For<ICommandSender>().Use(y => y.GetInstance<InProcessBus>());
+            For<IEventPublisher>().Use(y => y.GetInstance<InProcessBus>());
+            For<IHandlerRegistrar>().Use(y => y.GetInstance<InProcessBus>());
+            For<ISession>().HybridHttpOrThreadLocalScoped().Use<Session>();
+            For<IEventStore>().Singleton().Use<InMemoryEventStore>();
+            For<IRepository>().HybridHttpOrThreadLocalScoped().Use(y =>
+                    new CacheRepository(new Repository(y.GetInstance<IEventStore>()), y.GetInstance<IEventStore>()));
+
+            var profiles = from t in typeof(DefaultRegistry).Assembly.GetTypes()
+                           where typeof(Profile).IsAssignableFrom(t)
+                           select (Profile)Activator.CreateInstance(t);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in profiles)
+                {
+                    cfg.AddProfile(profile);
+                }
+            });
+
+            var mapper = config.CreateMapper();
+
+            For<IMapper>().Use(mapper);
+
+            //StackExchange.Redis
+            ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect("localhost");
+            For<IConnectionMultiplexer>().Use(multiplexer);
+        }
+    }
+}
--- WebApplication14/Startup.cs
+++ WebApplication14/Startup.cs
@@ -74,6 +74,7 @@ namespace WebApplication14
                     });
             
         }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILoggerFactory loggerFactory)
